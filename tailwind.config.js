0ed4261b9ac76fb231e223b73c8760e66ab749ad
Simone Salerno <58848053+eloquentarduino@users.noreply.github.com>
2024-07-11 10:49:48 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      'resources/views/**.blade.php',
+      'resources/views/**/*.blade.php',
       'resources/**/*.js',
   ],
   theme: {
